a0cbc82965f9c117a92449847a055401bd39b7b5
¡Duque <atanaelleonardo@gmail.com>
2024-05-01 19:08:27 -0300

    feat: "Add updatePartial method to UserService for partial user update"
    This commit adds a new method called `updatePartial` to the `UserService` class. This method is used to update partial user data in the database. It uses the

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { CreateUserDTO } from './dto/create-user.dto'
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto'
 import { UpdatePutUserDTO } from './dto/update-put-user.dto'
 
 @Injectable()
@@ -30,6 +31,17 @@ export class UserService {
   }
 
   async update(id: number, data: UpdatePutUserDTO) {
+    console.log(data)
+
+    return this.prisma.user.update({
+      where: { id },
+      data,
+    })
+  }
+
+  async updatePartial(id: number, data: UpdatePatchUserDTO) {
+    console.log(data)
+
     return this.prisma.user.update({
       where: { id },
       data,

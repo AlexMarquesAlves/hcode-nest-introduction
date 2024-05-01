c6a1be2ee370226424dfdf75b3f2a516b5fcd962
¡Duque <atanaelleonardo@gmail.com>
2024-05-01 16:02:14 -0300

    "Added PrismaService to UserService, created createUser function"
    This commit adds the PrismaService to the UserService constructor and creates a new async function called create that takes in a CreateUserDTO object with name, email

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CreateUserDTO } from './dto/create-user.dto'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create({ name, email, password }: CreateUserDTO) {}
+}

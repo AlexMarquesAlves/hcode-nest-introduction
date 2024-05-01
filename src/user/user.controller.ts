5f6e474bf87953c723ba606ccc77a78e85c3cdf4
¡Duque <atanaelleonardo@gmail.com>
2024-04-30 21:44:59 -0300

    Add GET method to UserController to retrieve user data
    This commit adds a new GET method to the UserController class, allowing for the retrieval of user data. The method accepts a Body parameter, which is returned in the response.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 
 @Controller('users')
 export class UserController {
   @Post()
   async createUser(@Body() body) {
+    return { body }
+  }
+
+  @Get()
+  async getUser(@Body() body) {
     return { body }
   }
 }

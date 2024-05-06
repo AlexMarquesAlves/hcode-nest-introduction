86e8f9bc50c429435d9d1d4da3d0edcbef301eb0
¡Duque <atanaelleonardo@gmail.com>
2024-05-06 01:42:39 -0300

    feat: "Change createToken method to return an object containing access token"
    This commit message describes the change in the `createToken` method of the `AuthService` class. Previously, the method returned the JWT token directly. With this

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,19 +14,21 @@ export class AuthService {
   ) {}
 
   async createToken(user: User) {
-    return this.JWTService.sign(
-      {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-      },
-      {
-        expiresIn: '7 days',
-        subject: String(user.id),
-        issuer: `login`,
-        audience: `users`
-      },
-    )
+    return {
+      accessToken: this.JWTService.sign(
+        {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+        {
+          expiresIn: '7 days',
+          subject: String(user.id),
+          issuer: `login`,
+          audience: `users`
+        },
+      )
+    }
   }
 
   async checkToken(token: string) {

cad4a6e698b8694bf7116bda8571f31dd65d7101
¡Duque <atanaelleonardo@gmail.com>
2024-05-05 23:13:52 -0300

    feat: "Add UserService to AuthModule and inject it into AuthController; register UserModule in AuthModule imports; export UserService in UserModule"

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { UserService } from 'src/user/user.service'
 import { AuthForgetDTO } from './dto/auth-forget.dto'
 import { AuthLoginDTO } from './dto/auth-login.dto'
 import { AuthRegisterDTO } from './dto/auth-register.dto'
@@ -6,11 +7,15 @@ import { AuthResetDTO } from './dto/auth-reset.dto'
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly userService: UserService) {}
+
   @Post('login')
   async login(@Body() body: AuthLoginDTO) {}
 
   @Post('register')
-  async register(@Body() body: AuthRegisterDTO) {}
+  async register(@Body() body: AuthRegisterDTO) {
+    return this.userService.create(body)
+  }
 
   @Post('forget')
   async forget(@Body() body: AuthForgetDTO) {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
+import { UserModule } from 'src/user/user.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 
 @Module({
-  imports: [JwtModule.register({ secret: `n#s!AFG%7sPBVWF=iJ,1B0Vl0BvvULtZ` })],
+  imports: [JwtModule.register({ secret: `n#s!AFG%7sPBVWF=iJ,1B0Vl0BvvULtZ` }), UserModule],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],

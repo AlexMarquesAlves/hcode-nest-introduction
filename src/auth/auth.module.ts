7c027905db71ee97aa90fd0ffc03cdad61333cc5
¡Duque <atanaelleonardo@gmail.com>
2024-05-05 23:25:30 -0300

    feat: Add PrismaModule to AuthModule imports and inject PrismaService in AuthService
    The diff adds the PrismaModule to the imports array in the AuthModule and injects the PrismaService in the AuthService constructor.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
+import { PrismaModule } from 'src/prisma/prisma.module'
 import { UserModule } from 'src/user/user.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 
 @Module({
-  imports: [JwtModule.register({ secret: `n#s!AFG%7sPBVWF=iJ,1B0Vl0BvvULtZ` }), UserModule],
+  imports: [
+    JwtModule.register({ secret: `n#s!AFG%7sPBVWF=iJ,1B0Vl0BvvULtZ` }),
+    UserModule,
+    PrismaModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [AuthService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly JWTService: JwtService) {}
+  constructor(
+    private readonly JWTService: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async createToken() {
     // return this.JWTService.sign()
@@ -12,4 +16,14 @@ export class AuthService {
   async checkToken(token: string) {
     // return this.JWTService.verify(token)
   }
+
+  async login(email: string, password: string) {
+    this.prisma.user.findFirst({
+      where: { email, password },
+    })
+  }
+
+  async forget() {}
+
+  async reset() {}
 }

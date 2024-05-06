85999ce61e9a7fe3bc3af2d95b3202cfb28d1cd4
¡Duque <atanaelleonardo@gmail.com>
2024-05-05 22:41:11 -0300

    feat: "Add AuthLoginDTO and AuthRegisterDTO to AuthController"
    This commit adds two new data transfer objects (DTOs), AuthLoginDTO and AuthRegisterDTO, to the AuthController in the 'auth' module. The controller's

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common'
+import { AuthLoginDTO } from './dto/auth-login.dto'
+import { AuthRegisterDTO } from './dto/auth-register.dto'
 
 @Controller('auth')
 export class AuthController {
   @Post('login')
-  async login() {}
+  async login(@Body body: AuthLoginDTO) {}
+
+  @Post('login')
+  async register(@Body body: AuthRegisterDTO) {}
 }

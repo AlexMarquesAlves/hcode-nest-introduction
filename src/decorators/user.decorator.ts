267f7266077aaf36abe4dcdd25538ff9ba705445
¡Duque <atanaelleonardo@gmail.com>
2024-05-07 02:41:49 -0300

    feat: "Update User decorator to accept a filter parameter for returning specific user properties"
    The User decorator has been updated to accept an optional filter parameter, allowing for the return of specific user properties instead of the entire user object. If no filter

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,11 +1,17 @@
 import { ExecutionContext, NotFoundException, createParamDecorator } from '@nestjs/common'
 
 export const User = createParamDecorator(
-  (_data: unknown, context: ExecutionContext) => {
+  (filter: string, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest().params.id
 
     if (request.user) {
-      return request.user
+
+      if (filter) {
+        return request.user[filter]
+      }else{
+        return request.user
+      }
+
     } else {
       throw new NotFoundException(`Usuário não encontrado no request. Use o AuthGuard para obter o usuário`)
     }

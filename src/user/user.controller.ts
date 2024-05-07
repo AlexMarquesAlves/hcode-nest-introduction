cb917bde91a454a6e887b981178502c7f455f92e
¡Duque <atanaelleonardo@gmail.com>
2024-05-07 12:53:43 -0300

    feat: Add roles decorator to UserController methods to restrict access to admin; no changes to functionality
    The diff adds a `@Roles(Role.Admin)` decorator to every method in the UserController class. This decorator restricts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common'
 import { ParamId } from 'src/decorators/param-id.decorator'
+import { Roles } from 'src/decorators/roles.decorator'
+import { Role } from 'src/enums/role.enum'
 import { CreateUserDTO } from './dto/create-user.dto'
 import { UpdatePatchUserDTO } from './dto/update-patch-user.dto'
 import { UpdatePutUserDTO } from './dto/update-put-user.dto'
@@ -10,36 +12,43 @@ import { UserService } from './user.service'
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Roles(Role.Admin)
   @Post()
   async create(@Body() data: CreateUserDTO) {
     return this.userService.create(data)
   }
 
+  @Roles(Role.Admin)
   @Post('many')
   async createMany() {
     return this.userService.createMany()
   }
 
+  @Roles(Role.Admin)
   @Get()
   async list() {
     return this.userService.list()
   }
 
+  @Roles(Role.Admin)
   @Get(':id')
   async show(@ParamId() id: number) {
     return this.userService.show(id)
   }
 
+  @Roles(Role.Admin)
   @Put(':id')
   async update(@Body() data: UpdatePutUserDTO, @ParamId() id) {
     return this.userService.update(id, data)
   }
 
+  @Roles(Role.Admin)
   @Patch(':id')
   async updatePartial(@Body() data: UpdatePatchUserDTO, @ParamId() id) {
     return this.userService.updatePartial(id, data)
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   async delete(@ParamId() id) {
     return this.userService.delete(id)

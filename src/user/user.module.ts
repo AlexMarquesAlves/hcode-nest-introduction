1a8fa6d04109055278f2cbc80ce61543a6d6f17f
¡Duque <atanaelleonardo@gmail.com>
2024-05-11 20:53:42 -0300

    "Update production environment variables, replace Prisma with TypeORM, update auth and user modules"
    This diff includes updates to the .env.production file with new environment variables, replacement of Prisma with TypeORM for database management,

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,12 +7,11 @@ import {
 } from '@nestjs/common'
 import { AuthModule } from 'src/auth/auth.module'
 import { UserIdCheckMiddleware } from 'src/middlewares/user-id-check.middleware'
-import { PrismaModule } from 'src/prisma/prisma.module'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
 
 @Module({
-  imports: [PrismaModule, forwardRef(() => AuthModule)],
+  imports: [ forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],

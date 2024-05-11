581bbba64bc6887aaac995aea2c7028ea1c11c80
¡Duque <atanaelleonardo@gmail.com>
2024-05-10 23:14:14 -0300

    feat: "Add MailerModule for email sending with NestJS modules and Pug adapter; configure transport, defaults, and template options"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { MailerModule } from '@nestjs-modules/mailer'
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter'
 import { Module, forwardRef } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
@@ -14,8 +16,25 @@ import { UserModule } from './user/user.module'
       ttl: 60,
       limit: 100,
     }),
+    // MODULES
     forwardRef(() => UserModule),
     forwardRef(() => AuthModule),
+
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>',
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],

ac42ad5cb91fbfc1fbb8219ecbdb72bd160ef195
¡Duque <atanaelleonardo@gmail.com>
2024-05-13 02:18:22 -0300

    chore: typeorm migrate create

---
+++ typeorm/migrations/1715577461909-Migrate.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migrate1715577461909 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

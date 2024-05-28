6f4eeaa2935a6dcefac586f59542f5e3891f6cf3
¡Duque <atanaelleonardo@gmail.com>
2024-05-27 21:38:31 -0300

    "Refactor UserService spec to use userRepositoryMock and add create method test"

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,34 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { userEntityList } from '../testing/user-entity-list.mock'
+import { userRepositoryMock } from '../testing/user-repository.mock'
 import { UserEntity } from './entity/user.entity'
 import { UserService } from './user.service'
 
 describe('UserService', () => {
   let userService: UserService
+  let userRepository: Repository<UserEntity>
 
   beforeEach(async () => {
-    const module: TestingModule = Test.createTestingModule({
-      providers: [
-        UserService,
-        {
-          provide: getRepositoryToken(UserEntity),
-          useValue: {
-            exist: jest.fn(),
-            create: jest.fn(),
-            save: jest.fn(),
-            find: jest.fn(),
-            findOneBy: jest.fn(),
-            update: jest.fn(),
-            delete: jest.fn(),
-          },
-        },
-      ],
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService, userRepositoryMock],
     }).compile()
 
-    userService = module.get<userService>(userService)
+    userService = module.get<UserService>(UserService)
+    userRepository = module.get(getRepositoryToken(UserEntity))
   })
 
-  it('should be a valid definition', () => {
+  it('should validate the definition successfully', () => {
     expect(userService).toBeDefined()
+    expect(userRepository).toBeDefined()
+  })
+
+  describe('Create', () => {
+    it('should use method create successfully', async () => {
+      jest.spyOn(userRepository, 'exist').mockResolvedValueOnce(false)
+
+      const result = await userService.create(createUserDTO)
+
+      expect(result).toEqual(userEntityList[0])
+    })
   })
 })

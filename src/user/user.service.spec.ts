ae6e89ee0f308c5468f93c61522dfa9b71ca127b
¡Duque <atanaelleonardo@gmail.com>
2024-05-28 14:38:42 -0300

    "Updated UserService spec: added Role enum, CreateUserDTO, and mocked create method input"

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { Role } from '../enums/role.enum'
 import { userEntityList } from '../testing/user-entity-list.mock'
 import { userRepositoryMock } from '../testing/user-repository.mock'
+import { CreateUserDTO } from './dto/create-user.dto'
 import { UserEntity } from './entity/user.entity'
 import { UserService } from './user.service'
 
@@ -25,10 +27,18 @@ describe('UserService', () => {
   })
 
   describe('Create', () => {
-    it('should use method create successfully', async () => {
+    it('should use create method successfully', async () => {
       jest.spyOn(userRepository, 'exist').mockResolvedValueOnce(false)
 
-      const result = await userService.create(createUserDTO)
+      const data: CreateUserDTO = {
+        birthAt: '2000-01-01',
+        email: 'joao@hcode.com.br',
+        name: 'Joao rangel',
+        password: '123456',
+        role: Role.User,
+      }
+
+      const result = await userService.create(data)
 
       expect(result).toEqual(userEntityList[0])
     })
